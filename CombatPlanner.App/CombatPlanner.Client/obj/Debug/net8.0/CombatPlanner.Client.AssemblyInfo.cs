f243b3c1fecb282c29aa1ddf2748c69c589f24ed
Ümit <uemit.tamyol@gmail.com>
2025-04-29 12:50:52 +0200

    ui finished with all styles etc

--- CombatPlanner.App/CombatPlanner.Client/obj/Debug/net8.0/CombatPlanner.Client.AssemblyInfo.cs
+++ CombatPlanner.App/CombatPlanner.Client/obj/Debug/net8.0/CombatPlanner.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CombatPlanner.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5babf35b9e5ee76c2edae6d7940857bec2f4e7e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca81f53c41d81a3d5843626a46d6c8467edbb5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CombatPlanner.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CombatPlanner.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
